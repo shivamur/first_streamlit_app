d42948c207cac419fa8676c1aa22cb61396b189e
shivamur <133325148+shivamur@users.noreply.github.com>
2023-05-12 12:14:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach, Rocket Smoothie') 
-stremalit.text('Hard-Boild Free-Range Egg')
+streamlit.text('Hard-Boild Free-Range Egg')
        
